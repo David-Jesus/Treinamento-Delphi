85ca5fe540ae00fad96f9a0e362e899a7d550164
Programacao WEB 03 <progweb03@sci10.local>
2021-10-18 09:45:50 -0300

    update

--- exercicio5-l4/uprincipal.dpr
+++ exercicio5-l4/uprincipal.dpr
@@ -9,18 +9,17 @@ uses
   DateUtils;
 
 Const
- Semana: array[1..7] of string = ('Segunda-Feira','Terça-Feira','Quarta-Feira','Quinta-Feira','Sexta-Feira','Sabado', 'Domingo');
+  Semana: array[1..7] of string = ('Segunda-Feira','Terça-Feira','Quarta-Feira','Quinta-Feira','Sexta-Feira','Sabado', 'Domingo');
 var
   wDia        ,
   wMes        ,
   wAno        ,
-  wContador   ,
-  dias_ano    : Integer;
+  wContador   : Integer;
   wDataInicial,
   wDataFinal  : TDateTime;
 
 begin
-  wDataInicial := EncodeDateTime(1900, 1, 12, 9, 10, 15, 0);
+  wDataInicial := EncodeDateTime(2000, 1, 12, 9, 10, 15, 0);
   wDataFinal   := EncodeDateTime(2021, 1, 12, 9, 10, 15, 0);
   wAno         := YearOf(wDataInicial);
   wDia         := 13;
@@ -38,8 +37,6 @@ begin
                  wContador := wContador + 1;
                  Writeln('Data: ' + DateToStr(EncodeDateTime(YearOf(wDataInicial), wMes, wDia, 0, 0, 0, 0)));
                end;
-
-
             wMes := wMes + 1;
           end;
       wAno := wAno + 1;
@@ -47,7 +44,7 @@ begin
       wMes := 1;
       end;
       Writeln('Total: ' + IntToStr(wContador));
-    Readln; //Deixa o console aberto até que seja clicado qualquer tecla
+    Readln; //Deixa o console aberto até que seja selecionado qualquer tecla para encerrar
   except
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
